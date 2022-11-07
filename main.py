38f1ce0aaefe02ad14c82bfb7e56cc487ed2ded3
Isaak <makarchuk008@gmail.com>
2022-11-07 13:29:48 +0400

    added basic functionality

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print('popa')
+x = int(input('Введите первое число: '))
+expression = input('Выберите операцию (+, -, *, /): ')
+y = int(input('Введите второе число: '))
+
+def sum():
+    return f"Результат {x+y}"
+
+def minus():
+    return f'Результат {x-y}'
+
+def multiplication():
+    return f"Результат {x*y}"
+
+def division():
+    return f"Результат {x/y}"
+
+def choise():
+    while input == 'Нет':
+        if expression == '+':
+            print(sum())
+        elif expression == '-':
+            print(minus())
+        elif expression == '*':
+            print(multiplication())
+        elif expression == '/':
+            print(division())
+        else:
+            print('Таких вычислений я не провожу')
+    print(input('Продолжить?'))
+
+
+
+print (choise())
